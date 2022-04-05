fffcd00e49a8ed0310f4d704468f407796cee17b
Cyprian Panicz <rupertgupica@gmail.com>
2022-04-05 09:50:13 +0200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut, quidem!
+        Enim autem, facere consequuntur quis eligendi quaerat, cum molestias
+        saepe quod obcaecati ipsam architecto dolor nam. Adipisci facilis eaque
+        sit eos voluptatem vero quasi, alias laborum, illo dolore cum eius.
+      </p>
+    </section>
+  );
+};
+
+export default About;
